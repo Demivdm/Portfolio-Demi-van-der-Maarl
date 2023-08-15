80e55e81cc516a776711ba149da4a2555a959712
Demivdm <demi.van.der.maarl@hva.nl>
2023-08-15 13:23:40 +0200

    color decrease with some js

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+  // JavaScript code to make the background color of h2 elements lighter
+function lightenH2BackgroundColors() {
+    const h2Elements = document.querySelectorAll('.timeline-root h2');
+  
+    h2Elements.forEach((h2, index) => {
+      const initialBackgroundColor = window.getComputedStyle(h2).backgroundColor;
+      const rgbValues = initialBackgroundColor.match(/\d+/g); // Extract RGB values
+      if (rgbValues.length === 3) {
+        const r = parseInt(rgbValues[0]);
+        const g = parseInt(rgbValues[1]);
+        const b = parseInt(rgbValues[2]);
+        const lightenFactor = 0.05 * index; // Adjust the lighten factor
+  
+        // Calculate new RGB values
+        const newR = Math.min(255, r + lightenFactor * 255);
+        const newG = Math.min(255, g + lightenFactor * 255);
+        const newB = Math.min(255, b + lightenFactor * 255);
+  
+        // Apply new background color to h2
+        h2.style.backgroundColor = `rgb(${newR}, ${newG}, ${newB})`;
+      }
+    });
+  }
+  
+  // Call the function once the DOM is loaded
+  document.addEventListener('DOMContentLoaded', lightenH2BackgroundColors);
+  
